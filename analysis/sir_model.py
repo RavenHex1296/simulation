e53588f477dcd1f99f989a9bcdd6561b044adc96
Cayden Lau <suanoo5196@gmail.com>
2021-02-15 21:47:34 +0000

    made hodgkin_huxley_neuron model

--- analysis/sir_model.py
+++ analysis/sir_model.py
@@ -1,38 +1,14 @@
-import matplotlib.pyplot as plt
+import sys
+sys.path.append('src')
+from euler_estimator import EulerEstimator
 
-susceptible = 1000
-infected = 1
-recovered = 0
-susceptible_values = []
-infected_values = []
-recovered_values = []
-times = []
+derivatives = {
+    'S': (lambda t, x: -0.0003 * x['S'] * x['I']),
+    'I': (lambda t, x: 0.0003 * x['S'] * x['I'] - 0.02 * x['I']),
+    'R': (lambda t, x: 0.02 * x['I'])
+}
+euler = EulerEstimator(derivatives)
+initial_values = {'S': 1000, 'I': 1, 'R': 0}
+initial_point = (0, initial_values)
 
-for n in range(226):
-    times.append(n)
-    times.append(n + 0.5)
-
-def clone(n):
-    cloned_value = 0
-    return cloned_value + n
-
-for _ in range(len(times)):
-    susceptible_values.append(susceptible)
-    infected_values.append(infected)
-    recovered_values.append(recovered)
-
-    cloned_susceptible = clone(susceptible)
-    cloned_infected = clone(infected)
-    
-    susceptible += -0.0003 * cloned_susceptible * cloned_infected
-    infected += 0.0003 * cloned_susceptible * cloned_infected - 0.02 * infected
-    recovered += 0.02 * cloned_infected
-
-plt.style.use('bmh')
-plt.plot(times, susceptible_values, label='Number of susceptible people')
-plt.plot(times, infected_values, label='Number of infected people')
-plt.plot(times, recovered_values, label='Number of recovered people')
-plt.legend(loc='best')
-plt.xlabel('Time Passed')
-plt.ylabel('Susceptible/Infected/Recovered People')
-plt.savefig('sir_model.png')
+euler.plot(initial_point, 1, 500)
